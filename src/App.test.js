7ed1f33a12feef4089ee10ddc983cc9d5a5ddbe0
Fran Nav <frannavmart@gmail.com>
2020-09-28 14:29:03 +0100

    Preparing mock for api test fecth

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import data from './data.json';
 
 describe("Rick and Morty App", () => {
+
+  beforeAll(() => jest.spyOn(window, "fetch"));
+
   it("Has a list of characters", () => {
     const { getByText } = render(<App />);
     for (let character of data.results) {
       expect(getByText(character.name)).toBeInTheDocument();
+    }
+  });
+
+  it("Shows a list of characters retrieved from an API",  async () => {
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+    });
+
+    render(<App />);
+
+    expect(window.fetch).toHaveBeenCalledWith("https://rickandmortyapi.com/api/character/");
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+
+    for (let character of data.results) {
+      expect(await screen.findByText(new RegExp(`${character.name}\\b`, "i"))).toBeInTheDocument();
     }
   });
 })
